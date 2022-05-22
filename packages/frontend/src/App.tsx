9f77041be69d13ac23762f3cae98d13df2749222
Batuhan Yenidunya <batuhan.yenidunya@getir.com>
2022-05-22 17:41:39 +0300

    feat(frontend): read from blockchain

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,70 +1,89 @@
 import {
-  useAccount,
-  useConnect,
-  useContractEvent,
-  useContractWrite,
-  useDisconnect,
-  useSigner,
-} from "wagmi";
-import { InjectedConnector } from "wagmi/connectors/injected";
-import { useContract } from "wagmi";
+    useAccount,
+    useConnect,
+    useContractEvent,
+    useContractRead,
+    useContractWrite,
+    useDisconnect,
+    useSigner,
+} from "wagmi"
+import { InjectedConnector } from "wagmi/connectors/injected"
+import { useContract } from "wagmi"
 import CoinflipContract from "secure-coinflip-blockchain/deployments/rinkeby/CoinflipV3.json"
 
 function App() {
-  const { data } = useAccount();
-  const { connect } = useConnect({
-    connector: new InjectedConnector(),
-  });
-  const { data: signer } = useSigner();
-  const { disconnect } = useDisconnect();
-  const contract = useContract({
-    addressOrName: CoinflipContract.address,
-    contractInterface: CoinflipContract.abi,
-    signerOrProvider: signer,
-  });
+    const { data } = useAccount()
+    const { connect } = useConnect({
+        connector: new InjectedConnector(),
+    })
+    const { data: signer } = useSigner()
+    const { disconnect } = useDisconnect()
+    const contract = useContract({
+        addressOrName: CoinflipContract.address,
+        contractInterface: CoinflipContract.abi,
+        signerOrProvider: signer,
+    })
 
-  useContractEvent(
-    {
-      addressOrName: CoinflipContract.address,
-      contractInterface: CoinflipContract.abi,
-    },
-    "RandomNumberFulfilled",
-    (event) => console.log(event)
-  );
+    useContractEvent(
+        {
+            addressOrName: CoinflipContract.address,
+            contractInterface: CoinflipContract.abi,
+        },
+        "RandomNumberFulfilled",
+        (event) => console.log(event)
+    )
 
-  const getValue = async () => {
-    const res = await contract.getResult(0);
-    console.log(res);
-  };
+    const {
+        data: readResult,
+        refetch,
+        isLoading,
+        isFetching,
+        isFetched,
+        fetchStatus,
+        status,
+    } = useContractRead(
+        {
+            addressOrName: CoinflipContract.address,
+            contractInterface: CoinflipContract.abi,
+        },
+        "log",
+        {
+            args: 1,
+        }
+    )
 
-  const getNewRandom = useContractWrite(
-    {
-      addressOrName: CoinflipContract.address,
-      contractInterface: CoinflipContract.abi,
-      signerOrProvider: signer,
-    },
-    "requestRandomWords",
-    {
-      onSettled: (res) => {
-        console.log("Settled: ", res);
-      },
-    }
-  );
+    const getNewRandom = useContractWrite(
+        {
+            addressOrName: CoinflipContract.address,
+            contractInterface: CoinflipContract.abi,
+            signerOrProvider: signer,
+        },
+        "requestRandomWords",
+        {
+            onSettled: (res) => {
+                console.log("Settled: ", res)
+            },
+        }
+    )
 
-  return (
-    <div>
-      {data ? (
+    return (
         <div>
-          Connected to {data.address}
-          <button onClick={() => disconnect()}>Disconnect</button>
-          <button onClick={getValue}>Get</button>;
-          <button onClick={() => getNewRandom.write()}>Get Random </button>;
+            {data ? (
+                <div>
+                    Connected to {data.address}
+                    <button onClick={() => disconnect()}>Disconnect</button>
+                    <button onClick={() => refetch()}>Get, {JSON.stringify(readResult)}</button>;
+                    <button onClick={() => getNewRandom.write()}>Get Random</button>;
+                    <div>
+                        isLoading {isLoading} | isFetching {isFetching} | isFetched {isFetched} |
+                        fetchStatus {fetchStatus} | status {status}
+                    </div>
+                </div>
+            ) : (
+                <button onClick={() => connect()}>Connect Wallet</button>
+            )}
         </div>
-      ) : (
-        <button onClick={() => connect()}>Connect Wallet</button>
-      )}
-    </div>
-  );
+    )
 }
 
-export default App;
+export default App
