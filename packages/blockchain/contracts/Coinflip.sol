feb74734e51518c0a27df13fa2e41fc3e2e04aef
Batuhan Yenidunya <batuhan.yenidunya@getir.com>
2022-05-26 19:46:51 +0300

    feat(blockchain): update contract

--- packages/blockchain/contracts/Coinflip.sol
+++ packages/blockchain/contracts/Coinflip.sol
@@ -6,38 +6,36 @@ import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 contract CoinflipV3 is VRFConsumerBaseV2 {
-  VRFCoordinatorV2Interface COORDINATOR;
-  event RandomNumberFulfilled(uint256);
+    VRFCoordinatorV2Interface COORDINATOR;
+    event RandomNumberFulfilled(uint256);
+    event RandomNumberRequested(uint256);
+    uint64 s_subscriptionId;
+    address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+    bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+    uint32 callbackGasLimit = 100000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords = 1;
 
-  uint64 s_subscriptionId;
-  address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
-  bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
-  uint32 callbackGasLimit = 100000;
-  uint16 requestConfirmations = 3;
-  uint32 numWords =  1;
+    mapping(uint256 => uint256) public log;
 
-  mapping(uint256 => uint256) public log;
+    constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        s_subscriptionId = subscriptionId;
+    }
 
-  constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
-    COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
-    s_subscriptionId = subscriptionId;
-  }
+    function requestRandomWords() external {
+        uint256 request_id = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+        emit RandomNumberRequested(request_id);
+    }
 
-  function requestRandomWords() external  {
-    COORDINATOR.requestRandomWords(
-      keyHash,
-      s_subscriptionId,
-      requestConfirmations,
-      callbackGasLimit,
-      numWords
-    );
-  }
-  
-  function fulfillRandomWords(
-    uint256 requestId,
-    uint256[] memory randomWords
-  ) internal override {
-    log[requestId] = randomWords[0];
-    emit RandomNumberFulfilled(requestId);
-  }
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        log[requestId] = randomWords[0];
+        emit RandomNumberFulfilled(requestId);
+    }
 }
